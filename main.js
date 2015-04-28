04536e3a7d3517de817f3d3c90e9fec2c287310f
Antonio Pierro <antonio.pierro@gmail.com>
2015-04-28 10:57:08 +0200

    Added laboratories query

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
 // main.js
-// ===================
+// =======
 
 var mysql       = require('mysql');
+var fs          = require('fs');
 var paramters   = require('./config/parameters.config');
+var queries       = require('./sql/services.sql');
+
 var connection  = mysql.createConnection(paramters.db_connection());
-var query       = require('./sql/services.sql');
+
+var writeJson = function (queryAction, fileName) {
+    connection.query(queryAction, function(err, rows, fields) {
+        if (err) {
+            throw err;
+        }
+        fs.writeFile(fileName, JSON.stringify(rows), function (err) {
+            if (err) {
+                throw err;
+            }
+            console.log(fileName + ' it is saved!');
+        });
+    });
+};
 
 connection.connect(function(err){
     if(!err) {
-        console.log("Database is connected ...");  
+        console.log('Database is connected ...');  
     } else {
-        console.log("Error connecting database ...");  
+        console.log('Error connecting database ...');  
     }
 });
 
-connection.query(query.services(), function(err, rows, fields) {
-    if (err) throw err;
-
-    // TODO should write a JSON file
-    console.log('The solution is: ', rows);
-});
+writeJson(queries.services(), 'services.data.json');
+writeJson(queries.instruments(), 'instruments.data.json');
+writeJson(queries.laboratories(), 'laboratories.data.json');
 
 connection.end();
--- sql/services.sql.js
+++ sql/services.sql.js
@@ -1,18 +1,32 @@
 // sql/services.sql.js
 // ===================
 module.exports = {
-  services: function () {
-    return "SELECT \
-        categoria_servizio.nome as name, \
-        servizi.nome as service_name, servizi.descrizione as service_description, \
-        laboratorio.nome as laboratory_name,  laboratorio.id_laboratorio as laboratory_id \
-        FROM \
-        servizi,laboratorio,categoria_servizio \
-        WHERE  servizi.id_laboratorio=laboratorio.id_laboratorio \
-        AND \
-        categoria_servizio.id=servizi.id_categoria_servizio \
-        AND \
-        categoria_servizio.nome = 'Analisi Chimico-Fisiche' \
-        LIMIT 5;";
-  }
+    services: function () {
+        return "SELECT \
+            categoria_servizio.nome as name, \
+            servizi.nome as service_name, servizi.descrizione as service_description, \
+            laboratorio.nome as laboratory_name,  laboratorio.id_laboratorio as laboratory_id \
+            FROM \
+            servizi,laboratorio,categoria_servizio \
+            WHERE  servizi.id_laboratorio=laboratorio.id_laboratorio \
+            AND \
+            categoria_servizio.id=servizi.id_categoria_servizio";
+    },
+    instruments: function () {
+        return "SELECT \
+            categoria_strumento.nome as name, strumenti.nome as instrument_name, \
+            strumenti.descrizione_dello_strumento as instrument_description, \
+            laboratorio.nome as laboratory_name,  laboratorio.id_laboratorio as laboratory_id \
+            FROM \
+            strumenti,laboratorio,categoria_strumento \
+            WHERE  strumenti.id_laboratorio=laboratorio.id_laboratorio \
+            AND \
+            categoria_strumento.id=strumenti.id_categoria";
+    },
+    laboratories: function () {
+        return "SELECT \
+            laboratorio.nome as laboratory_name, laboratorio.descrizione as laboratory_description \
+            FROM \
+            laboratorio";
+    }
 };
